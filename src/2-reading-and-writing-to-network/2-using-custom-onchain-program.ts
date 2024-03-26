5f8a2a66e5caf75e89131c0e6885e5a5fe9c996b
Shashank Daima <daimashashank10@gmail.com>
2024-03-26 10:31:38 +0530

    custom onchain done

--- src/2-reading-and-writing-to-network/2-using-custom-onchain-program.ts
+++ src/2-reading-and-writing-to-network/2-using-custom-onchain-program.ts
@@ -5,14 +5,28 @@ import { getKeypairFromEnvironment, airdropIfRequired } from "@solana-developers
 
 const payer = getKeypairFromEnvironment('SECRET_KEY')
 const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
-
+const programId = new web3.PublicKey('ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa')
+const pingProgramDataId =  new web3.PublicKey('Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod')
 const main = async () => {
-    const newBalance = await airdropIfRequired(
-        connection,
-        payer.publicKey,
-        1 * web3.LAMPORTS_PER_SOL,
-        0.5 * web3.LAMPORTS_PER_SOL,
-    );
+    const instruction = new web3.TransactionInstruction({
+      keys: [
+        {
+          pubkey: pingProgramDataId,
+          isSigner: false,
+          isWritable: true
+        },
+      ],
+      programId
+    })
     
+    const transaction = new web3.Transaction()
+    transaction.add(instruction)
+
+    const signature = await web3.sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer],
+    );
+    console.log(`✅ You can view your transaction on Solana Explorer at:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 }
 main();
